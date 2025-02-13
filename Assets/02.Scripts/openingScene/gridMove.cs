8a46f57dec3c6d617f8453cddab3f324798ed010
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-02-13 19:42:10 +0900

    🔨 Feat | 버그 픽스

--- Assets/02.Scripts/openingScene/gridMove.cs
+++ Assets/02.Scripts/openingScene/gridMove.cs
@@ -7,14 +7,11 @@ public class gridMove : MonoBehaviour
     private float moveSpeed = 1.2f;
     private float waitTime = 5.0f;
     private bool canMove = false;
-    [SerializeField] float extraDelay = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(WaitAndMove());
-        StartCoroutine(extraDelayTime());
-        StartCoroutine(MoveUpEvery100Seconds());
     }
 
     // Update is called once per frame
@@ -23,6 +20,10 @@ public class gridMove : MonoBehaviour
         if (canMove)
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            if (transform.position.y <= -120.0f)
+            {
+                transform.Translate(Vector3.up * 240.0f);
+            }
         }
     }
 
@@ -30,20 +31,5 @@ public class gridMove : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         canMove = true;
-    }
-
-    private IEnumerator extraDelayTime()
-    {
-        yield return new WaitForSeconds(extraDelay);
-    }
-
-    private IEnumerator MoveUpEvery100Seconds()
-    {
-        yield return new WaitForSeconds(extraDelay);
-        while (true)
-        {
-            yield return new WaitForSeconds(100.0f);
-            transform.Translate(Vector3.up * 240.0f);
-        }
     }
 }
